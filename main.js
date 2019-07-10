1801b18987ed99e13b0cb1d3ed23228898e5562f
DJ Knox <dhknox@gmail.com>
2019-07-10 18:45:03 -0400

    add <audio> to preview the audio of a result

--- main.js
+++ main.js
@@ -3,6 +3,11 @@
 const searchBar = document.querySelector('#searchBar');
 const searchButton = document.querySelector('#searchButton');
 const resultsDisplay = document.querySelector('#resultsDisplay');
+const resultPreview = document.querySelector('#resultPreview');
+const resultPreviewImg = document.querySelector('#resultPreviewImg');
+const resultPreviewAudio = document.querySelector('#resultPreviewAudio');
+const resultPreviewDescription = document.querySelector('#resultPreviewDescription');
+const results = document.querySelectorAll('.result');
 
 searchButton.addEventListener('click', function () {
     let fullUrl = createFullUrl();
@@ -11,9 +16,7 @@ searchButton.addEventListener('click', function () {
             return response.json();
         })
         .then(function(response) {
-            for (let result of response.results) {
-                addResultToResultsDisplay(result);
-            }
+            addResultsToDisplay(response.results);
         })
         .catch(function(error) {
             console.log('Request failed', error)
@@ -29,8 +32,56 @@ function createFullUrl() {
     return `https://itunes-api-proxy.glitch.me/search?term=${searchQuery}`;
 }
 
-function addResultToResultsDisplay(result) {
-    let resultDiv = document.createElement("div");
-    resultDiv.innerText = result.trackName;
-    resultsDisplay.appendChild(resultDiv);
+function clearResultsDisplay() {
+    resultsDisplay.innerHTML = '';
+}
+
+function addClickEventListenerToResult(resultDiv) {
+    resultDiv.addEventListener('click', function () {
+        // add img url for artwork
+        resultPreviewImg.src = resultDiv.querySelector('img[class=result-img]').src;
+        resultPreviewImg.removeAttribute('hidden');
+
+        // add preview url to audio tag
+        resultPreviewAudio.src = resultDiv.querySelector('a[class=result-preview-url]').href;
+        resultPreviewAudio.autoplay = true;
+
+        // add description
+        resultPreviewDescription.innerText = resultDiv.querySelector('p').innerText;
+        resultPreviewDescription.removeAttribute('hidden');
+
+    });
+}
+
+function addResultsToDisplay(results) {
+    clearResultsDisplay();
+    
+    for (let result of results) {
+        // create div to hold result
+        let resultDiv = document.createElement('div');
+        resultDiv.classList += 'result';
+
+        // create img for artwork
+        let resultImg = document.createElement('img');
+        resultImg.classList += 'result-img';
+        resultImg.src = result.artworkUrl100;
+        resultImg.alt = `${result.artistName} - ${result.trackName}`;
+        resultDiv.appendChild(resultImg);
+
+        // create p for description
+        let resultDescription = document.createElement('p');
+        resultDescription.innerText = `${result.artistName} - ${result.trackName}`;
+        resultDiv.appendChild(resultDescription);
+
+        // create hidden anchor for preview url
+        let resultPreviewUrl = document.createElement('a');
+        resultPreviewUrl.setAttribute('hidden', 'true');
+        resultPreviewUrl.classList += 'result-preview-url';
+        resultPreviewUrl.innerText = 'Preview Link';
+        resultPreviewUrl.href = result.previewUrl;
+        resultDiv.appendChild(resultPreviewUrl);
+
+        addClickEventListenerToResult(resultDiv);
+        resultsDisplay.appendChild(resultDiv);
+    }
 }
